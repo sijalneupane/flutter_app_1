dc6adeb100bfe565b206036648bc3a3d5adbe88e
sijalneupane <sijalneupane5@gmail.com>
2024-09-11 11:52:53 +0545

    facebook updated

--- lib/facebook.dart
+++ lib/facebook.dart
@@ -15,19 +15,36 @@ class Facebook extends StatelessWidget {
         style:GoogleFonts.roboto(
           fontSize: 26,
           fontWeight: FontWeight.bold,
-          color: const Color.fromARGB(255, 36, 140, 245)
+          color: const Color.fromARGB(255, 255, 255, 255)
         ),),
         actions: [
           CircleAvatar(
-            radius: 12,
-            backgroundColor: Colors.white.withOpacity(0.8),
+            radius: 11,
+            backgroundColor: Colors.white.withOpacity(0.9),
             child: Icon(Icons.add,
             size: 20,
             ),
-          ),
-        Icon(Icons.search),
+          ),SizedBox(width: 15,),
+        Icon(Icons.search ,color: Colors.white.withOpacity(0.9),),
+        SizedBox(width: 15,),
         Icon(FontAwesomeIcons.facebookMessenger,
-        size: 20,)
+        color: Colors.white.withOpacity(0.9),
+        size: 24,),
+        SizedBox(width: 15,)
+        ],
+      ),
+      body: const Column(
+        children: [
+          Row(mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Icon(Icons.home,color: Colors.white,),
+              Icon(Icons.tv_rounded,color: Colors.white,),
+              Icon(Icons.newspaper,color:Colors.white),
+              Icon(Icons.groups_2_outlined,color: Colors.white,),
+              Icon(Icons.notifications,color: Colors.white,),
+              Icon(Icons.menu,color: Colors.white,)
+            ],
+          )
         ],
       ),
     );
