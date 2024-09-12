92c33e16f778c75db4f7421a0f21deef3702b1fb
sijalneupane <sijalneupane5@gmail.com>
2024-09-12 22:27:25 +0545

    login updated

--- lib/login.dart
+++ lib/login.dart
@@ -14,9 +14,10 @@ class Login extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             SizedBox(
-              height: MediaQuery.of(context).size.height * 0.1,
+              height: MediaQuery.of(context).size.height * 0.12,
             ),
             TextFormField(
               decoration: InputDecoration(
@@ -44,16 +45,72 @@ class Login extends StatelessWidget {
             ),
             SizedBox(
               height: MediaQuery.of(context).size.height * 0.065,
-              width: MediaQuery.of(context).size.width * 0.5,
+              width: MediaQuery.of(context).size.width,
               child: ElevatedButton(
-                
                 onPressed: () {},
-                child: Text("Login",style: TextStyle(fontSize: 20),),
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.pink[200],
-                foregroundColor: Colors.white,
-
+                child: Text(
+                  "Login",
+                  style: TextStyle(fontSize: 20),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple[200],
+                  foregroundColor: Colors.white,
                 ),
               ),
+            ),
+            // SizedBox(height: MediaQuery.of(context).size.height*0.02,),
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.02,
+                bottom: MediaQuery.of(context).size.height * 0.03,
+              ),
+              child: Text(
+                "Forgot Password ?",
+                style: TextStyle(color: Colors.blue[300]),
+              ),
+            ),
+            Text("Don't have any account? Sign up"),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.height * 0.02),
+              child: Row(
+                children: [
+                  Expanded(child: Divider()),
+                  CircleAvatar(
+                    child: Text("OR"),
+                    backgroundColor: Colors.transparent,
+                  ),
+                  Expanded(child: Divider()),
+                ],
+              ),
+            ),
+            Text("Sign up wiht Social Networks"),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                        "https://upload.wikimedia.org/wikipedia/commons/6/6c/Facebook_Logo_2023.png")),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.08,
+                ),
+                CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTi-TGmA1kwrrCDuC7QtX3cojJb27aSXjE0Qw&s")),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.08,
+                ),
+                CircleAvatar(
+                    radius: 20,
+                    backgroundImage: NetworkImage(
+                        "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/x-social-media-round-icon.png")),
+               
+              ],
             )
           ],
         ),
