3005de3dcf15c24bb56eeabf9c76f408a0671727
sijalneupane <sijalneupane5@gmail.com>
2024-09-18 12:31:16 +0545

    Code added for Login form second for me, first for others

--- lib/login1.dart
+++ lib/login1.dart
@@ -14,7 +14,7 @@ class Login1 extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.all(15.0),
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
