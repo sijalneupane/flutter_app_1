f0196c37e454d65fc45ca368019a236b99930353
sijalneupane <sijalneupane5@gmail.com>
2024-11-10 11:39:54 +0545

    first day after dashain/tihar, revision of form

---
+++ lib/util/string_const1.dart
@@ -0,0 +1,12 @@
+String instagramStr="Instagram";
+String loginStr="Login Form";
+ 
+String nameStr="Enter your name";
+String addressStr="Enter your address";
+String passwordStr="Enter your password";
+
+String submitStr="Submit";
+
+String nameValidatorStr="Please enter the name";
+String addressValidatorStr="Please enter the address";
+String passwordValidatorStr="PLease enter the password";
