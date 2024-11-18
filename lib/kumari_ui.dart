0258040703364a2d857a664c2cab2af8e1aed482
sijalneupane <sijalneupane5@gmail.com>
2024-11-18 21:10:15 +0545

    tried to complete kumari bank but get confused

--- lib/kumari_ui.dart
+++ lib/kumari_ui.dart
@@ -2,9 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app_1/util/custom_container.dart';
 import 'package:flutter_app_1/util/custom_icon.dart';
 
-class KumariUi extends StatelessWidget {
+class KumariUi extends StatefulWidget {
   const KumariUi({super.key});
 
+  @override
+  State<KumariUi> createState() => _KumariUiState();
+}
+
+class _KumariUiState extends State<KumariUi> {
+  bool visible = false;
+  String accNo = "1234567899";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,31 +44,109 @@ class KumariUi extends StatelessWidget {
                         CustomIcon(icon: Icons.notifications)
                       ],
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
                     Row(
                       children: [
                         Container(
                           decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            border: Border.all(
-                              color: Colors.white,width: 2
-                            )
-                          ),
+                              shape: BoxShape.circle,
+                              border:
+                                  Border.all(color: Colors.white, width: 2)),
                           child: CircleAvatar(
+                            radius: 28,
                             backgroundColor: Colors.transparent,
-                            child: Text("SN",style: TextStyle(color: Colors.white),),
+                            child: Text(
+                              "SN",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
                           ),
+                        ),
+                        SizedBox(width: 20),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Good Morning",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            Text("Sijal",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500))
+                          ],
                         )
                       ],
                     ),
-                    CustomContainer(color: Colors.transparent),
+                    SizedBox(
+                      height: 30,
+                    ),
                     CustomContainer(
                       height: 110,
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 20.0, top: 10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              textBaseline: TextBaseline.alphabetic,
+                              children: [
+                                Icon(Icons.badge),
+                                SizedBox(
+                                  width: 10,
+                                ),
+                                Text("KUMARI SALARY SAVING")
+                              ],
+                            ),
+                            Text(visible ? accNo : "XXXXXXXXXX"),
+                            Row(
+                              children: [
+                                Text(visible ? "123 456" : "XXX XXX"),
+                                IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        visible = !visible;
+                                      });
+                                    },
+                                    icon: Icon(visible
+                                        ? Icons.visibility_off
+                                        : Icons.visibility))
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
                     ),
-                    CustomContainer(),
-                    CustomContainer(),
-                    CustomContainer(),
-                    CustomContainer(),
-                    CustomContainer(),
+                    // CustomContainer(child: ,),
+                    CustomContainer(
+                      serviceTitle: "My accounts",
+                      service1: "Accounts",
+                      service2: "Statement",
+                      service3: "Funds Transfer",
+                      service4: "Send Money",
+                    ),
+                    CustomContainer(
+                      serviceTitle: "Banking Services",
+                      service1: "Cash Deposit",
+                      service2: "Cheque Deposits",
+                      service3: "",
+                      service4: "",
+                    ),
+                    CustomContainer(
+                      serviceTitle: "Banking Services",
+                      service1: "",
+                      service2: "",
+                      service3: "",
+                      service4: "",
+                    ),
+                    // CustomContainer(),
+                    // CustomContainer(),
                   ],
                 ),
               ),
--- lib/util/custom_container.dart
+++ lib/util/custom_container.dart
@@ -2,21 +2,108 @@ import 'package:flutter/material.dart';
 
 class CustomContainer extends StatelessWidget {
   Color? color;
-  double? width,height;
+  double? width, height;
   Decoration? decoration;
-  CustomContainer({super.key,this.color,this.decoration,this.height,this.width});
+  Widget? child;
+  String? serviceTitle,service1,service2,service3,service4;
+
+  CustomContainer(
+      {super.key,
+      this.color,
+      this.decoration,
+      this.height,
+      this.width,
+      this.child, this.serviceTitle,
+      this.service1,this.service2,this.service3,this.service4,});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(bottom:15),
+      padding: const EdgeInsets.only(bottom: 15),
       child: Container(
-        decoration:decoration ?? BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: color ??Colors.white
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 0),
+          child: child ??Column(mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment:CrossAxisAlignment.start ,
+            children: [
+              Row(
+                children: [
+                  Text(serviceTitle ?? ""),
+                  Spacer(),
+                  Text("View All"),
+                  IconButton(onPressed: (){}, icon:Icon(Icons.arrow_forward_ios))
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 158, 53, 53),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
+                    Text(service1 ?? ''),
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 143, 54, 54),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
+                    Text(service2 ?? '',style: TextStyle(overflow: TextOverflow.clip),),
+
+                  ],
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 138, 45, 45),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
+                    Text(service3 ?? ''),
+                  ],
+                ),Column(
+                  children: [
+                    Container(
+                      height: 60,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 187, 70, 70),
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                    ),
+                    Text(service4 ?? '',softWrap: true,
+  overflow: TextOverflow.visible,maxLines: 2,),
+                  ],
+                )],
+              )
+            ],
+          ),
         ),
-              height:height ?? 140,
-              width: width?? MediaQuery.of(context).size.width,
+        decoration: decoration ??
+            BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: color ?? Colors.white),
+        height: height ?? 160,
+        width: width ?? MediaQuery.of(context).size.width,
       ),
     );
   }
